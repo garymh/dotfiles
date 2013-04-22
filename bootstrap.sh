84cb479ab9cf428f49058e7489ed0c0539e632dc
Gary Holtz <code@garyh.org>
2013-04-22 15:02:16 -0500

    oh, typos.

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,10 +14,11 @@ ln -s $(command pwd)/zsh/oh-my-zsh ~/.oh-my-zsh
 ln -s $(command pwd)/zsh ~/.zsh
 ln -s $(command pwd)/zsh/zshrc ~/.zshrc
 ln -s $(command pwd)/gitignore_global ~/.gitignore_global
-ln -s $(command pwd)/config ~/.ssh/config
 ln -s $(command pwd)/ackrc ~/.ackrc
 ln -s $(command pwd)/gemrc ~/.gemrc
 ln -s $(command pwd)/gitconfig ~/.gitconfig
 ln -s $(command pwd)/irbrc ~/.irbrc
+ln -s $(command pwd)/private/config ~/.ssh/config
 ln -s $(command pwd)/zsh/gary.zsh-theme zsh/oh-my-zsh/themes/gary.zsh-theme
-ln -s $(command pwd)/zsh/eifeon zsh/oh-my-zsh/custom/eifion
+mkdir zsh/oh-my-zsh/custom/plugins
+ln -s $(command pwd)/zsh/eifion zsh/oh-my-zsh/custom/plugins/eifion
