09a5df86bb52d423fcf3dbdf6cf65cda1cd57330
Gary Holtz <code@garyh.org>
2020-10-19 13:50:12 -0500

    [HAMMERSPOON]: Modifying grid resize visuals
    After learning about `hs.drawing`, I changed the
    visual indicator for the window about to be moved.
    
    Changing around a few of the key bindings as well.

--- home/hammerspoon/grid.lua
+++ home/hammerspoon/grid.lua
@@ -4,10 +4,36 @@ hs.grid.MARGINX             = 0
 hs.grid.MARGINY             = 0
 hs.window.animationDuration = 0.01
 
--- moom-like functionality
-moom = hs.hotkey.modal.new({"cmd"}, "f16")
-moom:bind({}, 'escape', function() moom:exit() end)
-moom:bind({}, 'space', function() moom:exit() end)
+local tall_screen    = hs.screen.find(458678080)
+local wide_screen    = hs.screen.find(458627443)
+local macbook_screen = hs.screen.find(2077748985)
+local hyper          = { "cmd", "alt", "ctrl", "shift" }
+
+function on_macbook()
+  if hs.screen.mainScreen():name() == "Color LCD" then
+    return true
+  else
+    return false
+  end
+end
+
+function drawBorder()
+  win = hs.window.focusedWindow()
+  if win ~= nil then
+    top_left      = win:topLeft()
+    size          = win:size()
+    global_border = hs.drawing.rectangle(hs.geometry.rect(top_left['x'], top_left['y'], size['w'], size['h']))
+    global_border:setFillColor({["red"]=0,["blue"]=1,["green"]=0})
+    global_border:setFill(true)
+    global_border:setAlpha(0.3)
+    global_border:show()
+  end
+end
+
+function redrawBorder()
+  global_border:delete()
+  drawBorder()
+end
 
 local function set_full_screen()
   win = hs.window.focusedWindow()
@@ -22,24 +48,42 @@ local function set_full_screen()
 end
 
 function moveWindowToDisplay(d)
-  return function()
-    local displays = hs.screen.allScreens()
-    local win = hs.window.focusedWindow()
-    win:moveToScreen(displays[d], false, true)
-  end
+  local displays = hs.screen.allScreens()
+  local win      = hs.window.focusedWindow()
+  win:moveToScreen(displays[d], false, true)
+  moom:exit()
 end
 
-local hyper = { "cmd", "alt", "ctrl", "shift" }
-hs.hotkey.bind(hyper, "1", moveWindowToDisplay(1))
-hs.hotkey.bind(hyper, "2", moveWindowToDisplay(2))
-hs.hotkey.bind(hyper, "3", moveWindowToDisplay(3))
-
 function adjust_moom (x, y, w, h)
+  -- hs.alert(hs.screen.mainScreen():id())
   hs.grid.set(hs.window.focusedWindow(), {x=x, y=y, w=w, h=h}, hs.screen.mainScreen())
   moom:exit()
 end
 
+-- moom-like functionality
+moom = hs.hotkey.modal.new({"cmd"}, "f16")
+moom:bind({}, 'escape', function() moom:exit() end)
+moom:bind({}, 'space',  function() moom:exit() end)
+
+function moom:entered()
+  -- hs.alert(hs.screen.mainScreen():id())
+  drawBorder()
+end
+
+function moom:exited()
+  global_border:delete()
+end
+
+function youtube()
+  moveWindowToDisplay(2)
+  hs.grid.maximizeWindow()
+end
+
 moom:bind({}, 'f', set_full_screen)
+
+moom:bind({}, '1', function() moveWindowToDisplay(1) end)
+moom:bind({}, '2', function() moveWindowToDisplay(2) end)
+moom:bind({}, 'y', youtube)
 moom:bind({}, 'h', function() adjust_moom(0, 0, 5,  10) end)
 moom:bind({}, 'g', function() adjust_moom(0, 0, 7,  10) end)
 moom:bind({}, 'l', function() adjust_moom(5, 0, 5,  10) end)
@@ -57,14 +101,14 @@ moom:bind({}, 's', function()
   moom:exit()
 end)
 
-moom:bind({}, 'DOWN', hs.grid.pushWindowDown)
-moom:bind({}, 'UP', hs.grid.pushWindowUp)
-moom:bind({}, 'LEFT', hs.grid.pushWindowLeft)
-moom:bind({}, 'RIGHT', hs.grid.pushWindowRight)
-moom:bind({"shift"}, 'UP', hs.grid.resizeWindowShorter)
-moom:bind({"shift"}, 'DOWN', hs.grid.resizeWindowTaller)
-moom:bind({"shift"}, 'RIGHT', hs.grid.resizeWindowWider)
-moom:bind({"shift"}, 'LEFT', hs.grid.resizeWindowThinner)
+moom:bind({}, 'DOWN',         function() hs.grid.pushWindowDown(); redrawBorder() end)
+moom:bind({}, 'UP',           function() hs.grid.pushWindowUp(); redrawBorder() end)
+moom:bind({}, 'LEFT',         function() hs.grid.pushWindowLeft(); redrawBorder() end)
+moom:bind({}, 'RIGHT',        function() hs.grid.pushWindowRight(); redrawBorder() end)
+moom:bind({"shift"}, 'UP',    function() hs.grid.resizeWindowShorter(); redrawBorder() end)
+moom:bind({"shift"}, 'DOWN',  function() hs.grid.resizeWindowTaller(); redrawBorder() end)
+moom:bind({"shift"}, 'RIGHT', function() hs.grid.resizeWindowWider(); redrawBorder() end)
+moom:bind({"shift"}, 'LEFT',  function() hs.grid.resizeWindowThinner(); redrawBorder() end)
 
 hs.hotkey.bind({"shift"}, 'F16', function()
   hs.hints.fontName = "Helvetica-BoldOblique"
@@ -72,7 +116,3 @@ hs.hotkey.bind({"shift"}, 'F16', function()
   moom:exit()
   hs.hints.windowHints()
 end)
-
-function moom:entered()
-  hs.alert("Resize")
-end
