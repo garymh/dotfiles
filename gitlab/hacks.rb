2e0c6b9f2219e0aa2054539b5b5401a411698522
Gary Holtz <code@garyh.org>
2020-11-19 15:30:42 -0600

    [GITLAB]: Adding some GitLab hacks

---
+++ gitlab/hacks.rb
@@ -0,0 +1,9 @@
+Amr = ApprovalMergeRequestRule
+Apr = ApprovalProjectRule
+Ars = ApprovalMergeRequestRuleSource
+Mr  = MergeRequest
+
+def dbg(b_ing = binding)
+  require 'pry'
+  b_ing.pry
+end
---
+++ gitlab/spec_helper.rb
@@ -0,0 +1,9 @@
+RSpec.configure do |config|
+  config.backtrace_exclusion_patterns = [
+    /\/lib\d*\/ruby\//,
+    /bin\//,
+    /gems/,
+    /spec\/spec_helper\.rb/,
+    /lib\/rspec\/(core|expectations|matchers|mocks)/
+  ]
+end
