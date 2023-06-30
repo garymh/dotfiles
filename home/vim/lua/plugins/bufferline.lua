ab47ee34d476a6afcbf8e8d1506a504963d18e41
Gary Holtz <code@garyh.org>
2023-06-30 18:04:10 -0500

    [VIM]: switching to cokeline. seems simple enough and doesn't mess with bindings

--- home/vim/lua/plugins/bufferline.lua
+++ home/vim/lua/plugins/bufferline.lua
@@ -1,73 +1,217 @@
 local M = {
-  "akinsho/bufferline.nvim",
-  event = "VeryLazy",
+  "willothy/nvim-cokeline",
+  event        = "VeryLazy",
   dependencies = { "kyazdani42/nvim-web-devicons" },
-  enabled = true,
+  enabled      = true,
 }
 
 function M.config()
-  -- local highlights = require("nord").bufferline.highlights({
-  --   italic = true,
-  --   bold = true,
-  -- })
+  local get_hex  = require('cokeline/utils').get_hex
+  local mappings = require('cokeline/mappings')
 
-  require("bufferline").setup({
-    options = {
-      -- view = "multiwindow",
-      numbers = "none",
-      color_icons = false,
-      -- show_buffer_default_icon = false,
-      separator_style = "thick",
-      -- number_style = "superscript",
-      -- mappings = false,
-      buffer_close_icon = "",
-      modified_icon = "●",
-      close_icon = "",
-      left_trunc_marker = "",
-      right_trunc_marker = "",
-      max_name_length = 15,
-      -- max_prefix_length = 1,
-      tab_size = 5,
-      name_formatter = function(buf) -- buf contains a "name", "path" and "bufnr"
-        -- remove extension from markdown files for example
-        -- if buf.name:match('%.md') then
-        return vim.fn.fnamemodify(buf.name, ":t:r")
-        -- end
+  local comments_fg = get_hex('Comment', 'fg')
+  local errors_fg   = get_hex('DiagnosticError', 'fg')
+  local warnings_fg = get_hex('DiagnosticWarn', 'fg')
+
+  local red    = vim.g.terminal_color_1
+  local yellow = vim.g.terminal_color_3
+
+  local rhs_components = {
+    branch = {
+      text = function()
+        if vim.b.gitsigns_head then
+          return "  " .. vim.b.gitsigns_head:sub(1, 20) .. " "
+        else
+          return ""
+        end
       end,
-      show_buffer_close_icons = false,
-      enforce_regular_tabs = false,
-      always_show_bufferline = false,
-      offsets = { { filetype = "NvimTree", text = "File Explorer" } },
-      sort_by = "id",
+      fg = "#B4BDC3",
+      bg = "#65435E",
+      style = function()
+        return 'bold,underline'
+      end
+    }
+  }
+
+  local components = {
+    space = {
+      text       = ' ',
+      truncation = { priority = 1 }
     },
-    highlights = highlights,
-    -- highlights = {
-    --   buffer_selected = {
-    --     fg = "#ffffff",
-    --     bg = "#5A93A9",
-    --     bold = true,
-    --     italic = true,
-    --   },
-    --   background = {
-    --     bg = "#2A3E41",
-    --   },
-    --   separator_selected = {
-    --     fg = "#152528",
-    --     bg = "#5A93A9",
-    --   },
-    --   separator = {
-    --     fg = "#152528",
-    --     bg = "#2A3E41",
-    --   },
+
+    two_spaces = {
+      text       = '  ',
+      truncation = { priority = 1 },
+    },
+
+
+    ee = {
+      text = function(buffer)
+        if string.find(buffer.path, "ee/", nil, true) then
+          return "EE"
+        else
+          return ""
+        end
+      end,
+
+      fg = function(buffer)
+        if buffer.is_focused then
+          return "#ffffff"
+        else
+          return nil
+        end
+      end,
+
+      bg = function(buffer)
+        if buffer.is_focused then
+          return get_hex('ErrorMsg', 'fg')
+        else
+          return nil
+        end
+      end,
+      style = 'bold',
+      -- truncation = { priority = 1 },
+    },
+    separator = {
+      text = function(buffer)
+        return buffer.index ~= 1 and '▏' or ''
+      end,
+      truncation = { priority = 1 }
+    },
+
+    devicon = {
+      text = function(buffer)
+        return
+            (mappings.is_picking_focus() or mappings.is_picking_close())
+            and buffer.pick_letter .. ' '
+            or buffer.devicon.icon
+      end,
+      fg = function(buffer)
+        return
+            (mappings.is_picking_focus() and yellow)
+            or (mappings.is_picking_close() and red)
+            or buffer.devicon.color
+      end,
+      style = function(_)
+        return
+            (mappings.is_picking_focus() or mappings.is_picking_close())
+            and 'italic,bold'
+            or nil
+      end,
+      truncation = { priority = 1 }
+    },
+
+    index = {
+      text = function(buffer)
+        return buffer.index .. ': '
+      end,
+      truncation = { priority = 1 }
+    },
+
+    unique_prefix = {
+      text = function(buffer)
+        return buffer.unique_prefix
+      end,
+      fg = comments_fg,
+      style = 'italic',
+      truncation = {
+        priority = 3,
+        direction = 'left',
+      },
+    },
+
+    filename = {
+      text = function(buffer)
+        return buffer.filename
+      end,
+      style = function(buffer)
+        return
+            ((buffer.is_focused and buffer.diagnostics.errors ~= 0)
+              and 'bold,underline')
+            or (buffer.is_focused and 'bold')
+            or (buffer.diagnostics.errors ~= 0 and 'underline')
+            or nil
+      end,
+      truncation = {
+        priority = 2,
+        direction = 'left',
+      },
+    },
+
+    diagnostics = {
+      text = function(buffer)
+        return
+            (buffer.diagnostics.errors ~= 0 and '  ' .. buffer.diagnostics.errors)
+            or (buffer.diagnostics.warnings ~= 0 and '  ' .. buffer.diagnostics.warnings)
+            or ''
+      end,
+      fg = function(buffer)
+        return
+            (buffer.diagnostics.errors ~= 0 and errors_fg)
+            or (buffer.diagnostics.warnings ~= 0 and warnings_fg)
+            or nil
+      end,
+      truncation = { priority = 1 },
+    },
+
+    close_or_unsaved = {
+      text = function(buffer)
+        return buffer.is_modified and '●' or ''
+      end,
+      fg = function(buffer)
+        return buffer.is_modified and "#BAD761" or nil
+      end,
+      delete_buffer_on_left_click = true,
+      truncation = { priority = 1 },
+    },
+  }
+
+  require('cokeline').setup({
+    show_if_buffers_are_at_least = 2,
+
+    buffers = {
+      -- filter_valid = function(buffer) return buffer.type ~= 'terminal' end,
+      -- filter_visible = function(buffer) return buffer.type ~= 'terminal' end,
+      new_buffers_position = 'next',
+    },
+
+    -- rendering = {
+    --   max_buffer_width = 30,
     -- },
+
+    default_hl = {
+      fg = function(buffer)
+        return
+            buffer.is_focused
+            and get_hex('normal', 'fg')
+            or get_hex('comment', 'fg')
+      end,
+      bg = function(buffer)
+        return
+            buffer.is_focused
+            and get_hex('BufferLineDuplicateSelected', 'fg')
+            or get_hex('ColorColumn', 'fg')
+      end,
+    },
+
+    components = {
+      components.space,
+      components.devicon,
+      -- components.index,
+      -- components.unique_prefix,
+      components.filename,
+      components.space,
+      components.ee,
+      components.diagnostics,
+      -- components.two_spaces,
+      components.space,
+      components.close_or_unsaved,
+    },
+
+    rhs = {
+      rhs_components.branch
+    }
   })
-
-  -- BufferLineCloseRight
-  -- BufferLineCloseLeft
-
-  -- vim.api.nvim_set_hl(0, "BufferCurrent",     { background = "#506591", foreground = "#ffffff" })
-  -- vim.api.nvim_set_hl(0, "BufferCurrentMod",  { background = "#506591", foreground = "#FF7570" })
-  -- vim.api.nvim_set_hl(0, "BufferCurrentSign", { background = "#506591", foreground = "#ffffff" })
 end
 
 return M
