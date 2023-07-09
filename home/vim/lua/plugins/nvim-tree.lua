be461a486f6c6b3b9500d4884a8c1af543652ebd
Gary Holtz <code@garyh.org>
2023-07-09 12:36:58 -0500

    [VIM]: using basic nvim-tree config

--- home/vim/lua/plugins/nvim-tree.lua
+++ home/vim/lua/plugins/nvim-tree.lua
@@ -5,48 +5,55 @@ local M = {
 }
 
 function M.config()
-
   require("nvim-tree").setup({
-    disable_netrw  = true,
-    hijack_netrw   = true,
-    open_on_tab    = false,
-    select_prompts = true,
-    hijack_cursor  = true,
-    update_cwd     = false,
-    diagnostics = {
-      enable = false,
-      icons = {
-        hint    = "",
-        info    = "",
-        warning = "",
-        error   = "",
-      },
-    },
-    update_focused_file = {
-      enable      = true,
-      update_cwd  = true,
-      ignore_list = {},
-    },
-    system_open = {
-      cmd  = nil,
-      args = {},
-    },
-    filters = {
-      dotfiles = false,
-      custom   = {},
-    },
+    sort_by = "case_sensitive",
     view = {
-      adaptive_size    = true,
-      hide_root_folder = false,
-      side             = "left",
-      mappings         = {
-        custom_only = false,
-        list        = {},
-      },
+      width = 30,
     },
+    renderer = {
+      group_empty = true,
+    },
+    -- disable_netrw  = true,
+    -- hijack_netrw   = true,
+    -- open_on_tab    = false,
+    -- select_prompts = true,
+    -- hijack_cursor  = true,
+    -- update_cwd     = false,
+    -- diagnostics = {
+    --   enable = false,
+    --   icons = {
+    --     hint    = "",
+    --     info    = "",
+    --     warning = "",
+    --     error   = "",
+    --   },
+    -- },
+    -- update_focused_file = {
+    --   enable      = true,
+    --   update_cwd  = true,
+    --   ignore_list = {},
+    -- },
+    -- system_open = {
+    --   cmd  = nil,
+    --   args = {},
+    -- },
+    -- filters = {
+    --   dotfiles = false,
+    --   custom   = {},
+    -- },
+    -- view = {
+    --   adaptive_size    = true,
+    --   hide_root_folder = false,
+    --   side             = "left",
+    --   mappings         = {
+    --     custom_only = false,
+    --     list        = {},
+    --   },
+    -- },
   }, {})
 
-  s_nmap("<space>a", vim.cmd.NvimTreeToggle, {}, "nvim Tree" )
+  s_nmap("<space>a", vim.cmd.NvimTreeToggle, {}, "nvim Tree")
 end
+
 --
 return M
