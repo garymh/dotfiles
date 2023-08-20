4aef0c5e66b23ec993d426be1a6b7e509ebf4b5c
Gary Holtz <code@garyh.org>
2023-08-20 11:37:51 -0500

    [FZF]: getting rid of script dependency

--- home/zsh/fzf.sh
+++ home/zsh/fzf.sh
@@ -102,8 +102,10 @@ if [[ -f ~/.fzf.zsh ]]; then
     fi
   }
 
-  bindkey '^g^g' fzf-git-checkout-branch
-  bindkey '^g^d' fzf-git-delete-branches
+  zle     -N   other-gg
+  bindkey '^g^g' other-gg
+  # bindkey '^g^g' fzf-git-checkout-branch
+  # bindkey '^g^d' fzf-git-delete-branches
 
   zle     -N   fzf-dotf
   bindkey '^F' fzf-dotf
@@ -111,5 +113,87 @@ if [[ -f ~/.fzf.zsh ]]; then
   zle     -N   fzf-nvim
   bindkey '^P' fzf-nvim
 
-  source ~/.fzf.zsh
+  is_in_git_repo() {
+    git rev-parse HEAD > /dev/null 2>&1
+  }
+
+  fzf-down() {
+    fzf --height 50% --min-height 20 --border --bind ctrl-/:toggle-preview "$@"
+  }
+
+  _gf() {
+    is_in_git_repo || return
+    git -c color.status=always status --short |
+    fzf-down -m --ansi --nth 2..,.. \
+      --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1})' |
+    cut -c4- | sed 's/.* -> //'
+  }
+
+  _gg() {
+    is_in_git_repo || return
+    git branch --sort=-committerdate --color=always | grep -v '/HEAD\s' | sort |
+    fzf-down --ansi --tac --preview-window right:70% \
+      --preview 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1)' |
+    sed 's/^..//' | cut -d' ' -f1 |
+    sed 's#^remotes/##'
+  }
+
+  _gt() {
+    is_in_git_repo || return
+    git tag --sort -version:refname |
+    fzf-down --multi --preview-window right:70% \
+      --preview 'git show --color=always {}'
+  }
+
+  _gh() {
+    is_in_git_repo || return
+    git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
+    fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+      --header 'Press CTRL-S to toggle sort' \
+      --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always' |
+    grep -o "[a-f0-9]\{7,\}"
+  }
+
+  _gr() {
+    is_in_git_repo || return
+    git remote -v | awk '{print $1 "\t" $2}' | uniq |
+    fzf-down --tac \
+      --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1}' |
+    cut -d$'\t' -f1
+  }
+
+  _gs() {
+    is_in_git_repo || return
+    git stash list | fzf-down --reverse -d: --preview 'git show --color=always {1}' |
+    cut -d: -f1
+  }
+
+  join-lines() {
+  local item
+  while read item; do
+    echo -n "${(q)item} "
+  done
+}
+
+  () {
+    local c
+    for c in $@; do
+      eval "fzf-g$c-widget() { local result=\$(_g$c | join-lines); zle reset-prompt; LBUFFER+=\$result }"
+      eval "zle -N fzf-g$c-widget"
+      eval "bindkey '^g^$c' fzf-g$c-widget"
+    done
+  } f t r h s
+
+  fzf-gg-widget() {
+    local result=$(_gg)
+
+    if [[ $result == "" ]]; then
+    else
+      git checkout $result
+    fi
+    zle reset-prompt;
+  }
+
+  zle -N fzf-gg-widget
+  bindkey '^g^g' fzf-gg-widget
 fi
