f1f3d3a183c7ce3845517d1e9083e28de83ce48c
Gary Holtz <code@garyh.org>
2026-04-18 13:22:53 -0500

    chore(vim): align assignments and simplify commented autocmd in help.lua

--- home/vim/ftplugin/help.lua
+++ home/vim/ftplugin/help.lua
@@ -1,18 +1,13 @@
-vim.o.list = false
-vim.o.colorcolumn = ""
+vim.o.list          = false
+vim.o.colorcolumn   = ""
 vim.o.concealcursor = "nc"
-vim.o.number = nil
+vim.o.number        = nil
 
--- https://www.reddit.com/r/neovim/comments/10383z1/open_help_in_buffer_instead_of_split/
--- vim.api.nvim_create_autocmd('BufWinEnter', {
---   pattern = '*',
---   callback = function(event)
---     if vim.bo[event.buf].filetype == 'help' then
---       vim.cmd.only()
---       vim.bo[event.buf].buflisted = true
---     end
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "help",
+--   callback = function()
+--     vim.cmd("wincmd o")
 --   end,
---   desc = "Open help pages in a listed buffer in the current window."
 -- })
 
 vim.keymap.set("n", "<cr>", "<c-]>", { buffer = true, noremap = true })
