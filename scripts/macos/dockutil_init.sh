475cc691f0536786d1c4c4f1b7d63022613cd35d
Gary Holtz <code@garyh.org>
2026-06-12 00:22:39 -0500

    feat(macos): adjust dock apps

--- scripts/macos/dockutil_init.sh
+++ scripts/macos/dockutil_init.sh
@@ -1,7 +1,7 @@
 dockutil --remove all
 dockutil --add "/Applications/Safari.app"
-dockutil --add "/Applications/Helium.app"
-dockutil --add "/Applications/Firefox Developer Edition.app.app"
+dockutil --add "/Applications/Firefox.app"
+dockutil --add "/Applications/Firefox Developer Edition.app"
 dockutil --add '' --type spacer --section apps --after "Firefox Developer Edition"
 dockutil --add "/System/Applications/Mail.app"
 dockutil --add "/System/Applications/Messages.app"
@@ -9,9 +9,7 @@ dockutil --add '' --type spacer --section apps --after Messages
 dockutil --add "/Applications/Ghostty.app"
 dockutil --add "/Applications/OmniFocus.app"
 dockutil --add "/Applications/Discord.app"
-dockutil --add "/Applications/Slack.app"
 dockutil --add '' --type spacer --section apps --after Slack
-dockutil --add "$HOME/Applications/Gmail.app"
 dockutil --add "/Applications/Calendar.app"
 dockutil --add "/Applications/System Settings.app"
-dockutil --add "$HOME/Downloads/Personal Folder/Downloads" --view list --display folder --sort name
+dockutil --add "$HOME/Downloads" --view list --display folder --sort name
